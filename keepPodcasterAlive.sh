311fd42196b19fb253394b026a5321b3726a89ee
fc92 <florent.carre@free.fr>
2020-11-09 23:55:31 +0100

    remove static user path, add radio URL

--- keepPodcasterAlive.sh
+++ keepPodcasterAlive.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 # call this script from the crontab to (re)start the podcaster automatically
-# TO DO: remove static user
 NB_PODCASTER=`ps aux | grep "podcaster.py" | grep -v grep | wc -l`
 echo $NB_PODCASTER
 if [ $NB_PODCASTER -eq 0 ]
 then 
-	{
-		/usr/bin/python3 /home/f/podcaster.py &
-	} 2>&1 > /home/f/podcaster.log
-	date>/home/f/podcaster-age.log
+	/bin/date >$HOME/podcaster-age.log
+	/usr/bin/python3 -u $HOME/podcaster.py 2>&1 >$HOME/podcaster.log &
 else 
-	echo ok>>/home/f/podcaster-age.log
+	echo ok>>$HOME/podcaster-age.log
 fi
