b34e901f701d2a5899ac4f14d03326231dcfeebb
fc92 <florent.carre@free.fr>
2020-11-25 10:25:11 +0100

    remove .vscode directory

--- connectBT.sh
+++ connectBT.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # in case connection to bluetooth speaker is not established, try to reconnect
-# TO DO: remove static BT address
-BT=00:19:F5:86:38:90
+# to device in .asoundrc (only one supported; lines commented must begin with #)
+BT=`cat .asoundrc | grep -v ^# | grep device | cut -d '"' -f 2`
 IS_CONNECTED=`bluetoothctl info $BT | grep "Connected: yes" | wc -l`
 if [ $IS_CONNECTED -eq 0 ]
 then
+	echo Connecting to $BT
 	bluetoothctl connect $BT
+else
+	echo Already connected to BT device
 fi
--- updatePodcasts.sh
+++ updatePodcasts.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# call this script from the crontab to (re)start the podcaster automatically
-NB_PODCASTER=`ps aux | grep gpo | grep -v grep | wc -l`
-echo $NB_PODCASTER
-if [ $NB_PODCASTER -eq 0 ]
+# call this script from the crontab to update podcasts only if no other update is already running
+NB_GPO=`ps aux | grep gpo | grep -v grep | wc -l`
+echo $NB_GPO
+if [ $NB_GPO -eq 0 ]
 then 
 	date > $HOME/gpodder-cron.log
 	$HOME/gpodder/bin/gpo update 2>&1 >> $HOME/gpodder-cron.log
